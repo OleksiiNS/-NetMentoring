304c720a92fee482c881d6cc41dce7fe7c8cba82
Oleksii Neshcheret <Oleksii.Neshcheret@lseg.com>
2023-04-09 20:21:09 +0200

    4. Write a program which recursively creates 10 threads.

--- 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,25 +10,56 @@
  */
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        public static Semaphore semaphore = new Semaphore(1, 1);
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
             Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
             Console.WriteLine("Implement all of the following options:");
             Console.WriteLine();
+            
             Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
-            Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
+            int number = 10;
+            var parent = new Thread(()=> Decrement(number));
+            parent.Start();
+            parent.Join();
 
             Console.WriteLine();
-
-            // feel free to add your code
-
+            Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
+            semaphore.WaitOne();
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DecrementPool), number);
+            semaphore.Release();
             Console.ReadLine();
+        }
+
+        static void Decrement(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine(number);
+                var task = new Thread(()=> Decrement(number-1));
+                task.Start();
+                task.Join();
+            };
+        }
+
+        static void DecrementPool(object value)
+        {
+            int number = (int)value;
+            if (number > 0)
+            {
+                Console.WriteLine(number);
+                semaphore.WaitOne();
+                ThreadPool.QueueUserWorkItem(new WaitCallback(DecrementPool), number-1);
+                semaphore.Release();
+            };
         }
     }
 }
