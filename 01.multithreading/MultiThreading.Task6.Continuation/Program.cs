ef0054990eadd15423bd1d433dc248ccf32146c4
Oleksii Neshcheret <Oleksii.Neshcheret@lseg.com>
2023-04-10 16:23:00 +0200

    Create a Task and attach continuations to it according to the following criteria

--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -21,9 +23,29 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
+            var source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
 
-            // feel free to add your code
+            var task = Task.Run(()=> {
+                var date = DateTime.Now; 
+                Console.WriteLine($"Parent task. Date is {date.ToLongDateString()}");
+            });
+            task.ContinueWith( _ => { 
+                Console.WriteLine("Continuation task should be executed regardless of the result of the parent task."); 
+                }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            task.ContinueWith( _ =>{ 
+                Console.WriteLine("Continuation task should be executed when the parent task finished without success.");
+                }, TaskContinuationOptions.OnlyOnFaulted);
+            task.ContinueWith((i, token) => { 
+                Console.WriteLine("Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation."); 
+                }, null, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.FromCurrentSynchronizationContext());
+            task.ContinueWith( (i, token) =>{ 
+                Console.WriteLine("Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
+                }, null,token, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.FromCurrentSynchronizationContext());
 
+            
+            task.Wait();
             Console.ReadLine();
         }
     }
